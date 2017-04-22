81c98790582891ec5f4f908413879e05178a431e
Austin Adamson <austin.l.adamson@gmail.com>
2017-04-22 16:36:30 -0500

    single-event and components

---
+++ wp-content/themes/bb-theme-child/content-single-event.php
@@ -0,0 +1,77 @@
+<?php
+
+$show_thumbs = FLTheme::get_setting('fl-posts-show-thumbs');
+$meta = get_post_meta($post->ID);
+?>
+<article <?php post_class( 'fl-post' ); ?> id="fl-post-<?php the_ID(); ?>" itemscope itemtype="http://schema.org/BlogPosting">
+
+	<?php if(has_post_thumbnail() && !empty($show_thumbs)) : ?>
+		<?php if($show_thumbs == 'above-title') : ?>
+		<div class="fl-post-thumb">
+			<?php the_post_thumbnail('large', array('itemprop' => 'image')); ?>
+		</div>
+		<?php endif; ?>
+	<?php endif; ?>
+
+	<header class="fl-post-header">
+		<h1 class="fl-post-title" itemprop="headline">
+			<?php the_title(); ?>
+			<?php edit_post_link( _x( 'Edit', 'Edit post link text.', 'fl-automator' ) ); ?>
+		</h1>
+		<?php
+        // Insert Event Data
+        if ( isset($meta['event_date'][0]) && !empty($meta['event_date'][0]) ) {
+            echo "Date: " . esc_html($meta['event_date'][0]);
+        }
+        if ( isset($meta['start_time'][0]) && !empty($meta['start_time'][0]) ) {
+            echo "<br />Start Time: " . esc_html( date("g:i a", strtotime($meta['start_time'][0])));
+        }
+        if ( isset($meta['end_time'][0]) && !empty($meta['end_time'][0]) ) {
+            echo "<br />End Time: " . esc_html( date("g:i a", strtotime($meta['end_time'][0])));
+        }
+        ?>
+	</header><!-- .fl-post-header -->
+
+	<?php if(has_post_thumbnail() && !empty($show_thumbs)) : ?>
+		<?php if($show_thumbs == 'above') : ?>
+		<div class="fl-post-thumb">
+			<?php the_post_thumbnail('large'); ?>
+		</div>
+		<?php endif; ?>
+
+		<?php if($show_thumbs == 'beside') : ?>
+		<div class="row">
+			<div class="col-md-3 col-sm-3">
+				<div class="fl-post-thumb">
+					<?php the_post_thumbnail('thumbnail'); ?>
+				</div>
+			</div>
+			<div class="col-md-9 col-sm-9">
+		<?php endif; ?>
+	<?php endif; ?>
+
+	<div class="fl-post-content clearfix" itemprop="text">
+		<?php
+
+		the_content();
+
+		wp_link_pages( array(
+			'before'         => '<div class="fl-post-page-nav">' . _x( 'Pages:', 'Text before page links on paginated post.', 'fl-automator' ),
+			'after'          => '</div>',
+			'next_or_number' => 'number'
+		) );
+
+		?>
+	</div><!-- .fl-post-content -->
+
+	<?php if(has_post_thumbnail() && $show_thumbs == 'beside') : ?>
+		</div>
+	</div>
+	<?php endif; ?>
+
+	<?php //FLTheme::post_bottom_meta(); ?>
+	<?php FLTheme::post_navigation(); ?>
+	<?php comments_template(); ?>
+
+</article>
+<!-- .fl-post -->
--- wp-content/themes/bb-theme-child/functions.php
+++ wp-content/themes/bb-theme-child/functions.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+* dump function for debug
+*/
+if (!function_exists('dump')) {
+    function dump ($var, $label = 'Dump', $echo = TRUE) {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+        $output = '<pre style="background: #FFFEEF; color: #000; border: 1px dotted #000; padding: 10px; margin: 10px 0; text-align: left; width: 100% !important; font-size: 12px !important;">' . $label . ' => ' . $output . '</pre>';
+        if ($echo == TRUE) {
+            echo $output;
+        }
+        else {
+            return $output;
+        }
+    }
+}
+if (!function_exists('dump_exit')) {
+    function dump_exit($var, $label = 'Dump', $echo = TRUE) {
+        dump ($var, $label, $echo);exit;
+    }
+}
+
 // Defines
 define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
 define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
